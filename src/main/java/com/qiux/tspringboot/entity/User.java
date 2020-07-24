a3fad0e0f1cedeb6c3c48d25ed08f76f0dd5dea7
qiuxian <qiuxian@sdbattery.com>
2020-07-24 15:49:40 +0800

    测试nacos配置

---
+++ src/main/java/com/qiux/tspringboot/entity/User.java
@@ -0,0 +1,24 @@
+package com.qiux.tspringboot.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.Serializable;
+
+/**
+ * @author qiuxian
+ * @date 2020/7/24
+ */
+@ConfigurationProperties("user")
+@Configuration
+@RefreshScope
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 404733442753945595L;
+    private String id;
+
+}
