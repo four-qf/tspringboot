006253786e46b98a900fa4794424408ef7a12cb4
qiux.mac <qiux166036@gmail.com>
2022-02-13 11:13:20 +0800

    factoryBean

---
+++ src/main/java/com/qiux/tspringboot/service/TestService.java
@@ -0,0 +1,13 @@
+package com.qiux.tspringboot.service;
+
+/**
+ * @author qiux
+ * @Date 2022/2/12
+ * @since
+ */
+public class TestService {
+
+    public TestService() {
+        System.out.println("TestService constant");
+    }
+}
---
+++ src/main/java/com/qiux/tspringboot/test/createobject/CustomerFactoryBean.java
@@ -0,0 +1,41 @@
+package com.qiux.tspringboot.test.createobject;
+
+import com.qiux.tspringboot.service.TestService;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author qiux
+ * @Date 2022/2/12
+ * @since
+ */
+@Component
+public class CustomerFactoryBean implements FactoryBean<TestService> {
+    @Override
+    public TestService getObject() throws Exception {
+        return new TestService();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return TestService.class;
+    }
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.qiux.tspringboot.test.createobject");
+        System.out.println("容器启动完成");
+        TestService testService = applicationContext.getBean(TestService.class);
+        System.out.println(testService);
+        Object customerFactoryBean = applicationContext.getBean("customerFactoryBean");
+        System.out.println(customerFactoryBean);
+
+        Object bean = applicationContext.getBean("&customerFactoryBean");
+        System.out.println(bean);
+
+    }
+}
