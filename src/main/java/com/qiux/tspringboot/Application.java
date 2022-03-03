1f1a2856d53b2018b26153608e86f86aaca31605
qiux.mac <qiux166036@gmail.com>
2022-03-03 22:34:31 +0800

    Hystrix

--- src/main/java/com/qiux/tspringboot/Application.java
+++ src/main/java/com/qiux/tspringboot/Application.java
@@ -2,9 +2,12 @@ package com.qiux.tspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableHystrix
+@EnableDiscoveryClient
 public class Application {
 
 
--- src/main/java/com/qiux/tspringboot/ctrl/IndexController.java
+++ src/main/java/com/qiux/tspringboot/ctrl/IndexController.java
@@ -1,5 +1,8 @@
 package com.qiux.tspringboot.ctrl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +18,25 @@ public class IndexController {
     public Object index() {
         return "hello";
     }
+
+    @HystrixCommand(fallbackMethod="hystrixFallBack",
+            commandProperties ={
+                   @HystrixProperty(name="circuitBreaker.requestVolumeThreshold",value="10"),
+                   @HystrixProperty(name="circuitBreaker.errorThresholdPercentage",value="20")
+            }
+    )
+    @RequestMapping("/getnum/{num}")
+    public String testHystrix(@PathVariable("num") int num) {
+        if(num<0){
+            throw new RuntimeException("****id 不能负数");
+        }
+        return String.valueOf(num);
+    }
+
+    public String hystrixFallBack(@PathVariable("num") int num) {
+        return "num不能为负数";
+    }
+
+
 
 }
