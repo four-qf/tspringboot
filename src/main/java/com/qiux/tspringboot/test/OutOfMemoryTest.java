eceee2e80f29a16cdcee61e0307b9dcd9177fed9
qiux.mac <qiux166036@gmail.com>
2022-03-03 20:54:06 +0800

    测试VM GC跟踪

---
+++ src/main/java/com/qiux/tspringboot/test/OutOfMemoryTest.java
@@ -0,0 +1,21 @@
+package com.qiux.tspringboot.test;
+
+import java.util.Random;
+
+/**
+ * @author qiux
+ * @Date 2022/3/3
+ * @since
+ */
+public class OutOfMemoryTest  {
+
+    public static void main(String[] args) {
+
+        String str = "";
+        while (true) {
+            str += str + new Random().nextInt(999999999);
+        }
+
+    }
+
+}
