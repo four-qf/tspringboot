121883f12e78304fbfb8281641b08be8bdc85337
qiux.mac <qiux166036@gmail.com>
2022-03-15 11:23:55 +0800

    WebConfigurer

---
+++ src/main/java/com/qiux/tspringboot/interceptor/WebConfigurer.java
@@ -0,0 +1,25 @@
+package com.qiux.tspringboot.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author qiux
+ * @Date 2022/3/12
+ * @since
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor);
+    }
+}
