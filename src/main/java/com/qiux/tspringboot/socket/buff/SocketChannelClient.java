767326623ec3ff17a2f2474701cb66b10c1fc6a3
qiux.mac <qiux166036@gmail.com>
2022-03-20 11:16:37 +0800

    socket channle

--- src/main/java/com/qiux/tspringboot/socket/buff/SocketChannelClient.java
+++ src/main/java/com/qiux/tspringboot/socket/buff/SocketChannelClient.java
@@ -1,6 +1,54 @@
-package com.qiux.tspringboot.socket.buff;/**
+package com.qiux.tspringboot.socket.buff;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+/**
  * @author qiux
  * @Date 2022/3/19
  * @since
- */public class SocketChannelClient {
+ */
+public class SocketChannelClient {
+
+    public static void main(String[] args) {
+
+        SocketChannel socketChannel = null;
+        try {
+
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            boolean flag = socketChannel.connect(new InetSocketAddress("localhost", 9999));
+            System.out.println(flag);
+            if (!flag) {
+                 while (!socketChannel.finishConnect()) {
+                     System.out.println("---------继续链接");
+                 }
+            }
+//            byte[] bytes = "你好".getBytes();
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNextLine()) {
+                String msg = scanner.next();
+                byte[] bytes = msg.getBytes();
+                ByteBuffer byteBuffer = ByteBuffer.allocate(bytes.length);
+                byteBuffer.put(bytes);
+                byteBuffer.flip();
+                socketChannel.write(byteBuffer);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (socketChannel != null) {
+                try {
+                    socketChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
--- src/main/java/com/qiux/tspringboot/socket/buff/SocketChannelServer.java
+++ src/main/java/com/qiux/tspringboot/socket/buff/SocketChannelServer.java
@@ -1,6 +1,76 @@
-package com.qiux.tspringboot.socket.buff;/**
+package com.qiux.tspringboot.socket.buff;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
  * @author qiux
  * @Date 2022/3/19
  * @since
- */public class ServerSocketChannelUtil {
+ */
+public class SocketChannelServer {
+
+    public static void main(String[] args) {
+
+        ServerSocketChannel serverChannel = null;
+        try {
+
+            serverChannel = ServerSocketChannel.open();
+            serverChannel.configureBlocking(false);
+            serverChannel.bind(new InetSocketAddress("localhost", 9999));
+            Selector selector = Selector.open();
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            while (selector.select() > 0) {
+
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+
+                while (iterator.hasNext()) {
+
+                    SelectionKey key = iterator.next();
+                    if (key.isAcceptable()) {
+                        SocketChannel accept = serverChannel.accept();
+                        accept.configureBlocking(false);
+                        accept.register(selector, SelectionKey.OP_READ);
+                    }
+
+                    if (key.isReadable()) {
+
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                        SocketChannel channel = (SocketChannel) key.channel();
+                        int len = 0;
+                        while ( (len = channel.read(byteBuffer) ) > 0) {
+                            byteBuffer.flip();
+                            String msg = new String(byteBuffer.array(),0, len);
+                            System.out.println("------------msg:" + msg);
+                            byteBuffer.clear();
+                        }
+
+
+                    }
+                    iterator.remove();
+
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (serverChannel != null) {
+                try {
+                    serverChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
 }
